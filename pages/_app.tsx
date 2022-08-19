e1cd4d31ceb76455081197110243bb9c173d91ac
Alper Bayraktar <alpervecoding58@gmail.com>
2022-08-19 21:40:13 +0300

    initial commit with superplate

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "@definitions/chakra/theme";
+import "@styles/global.css";
+import { appWithTranslation } from "@i18n";
+
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+}
+
+export default MyApp;
